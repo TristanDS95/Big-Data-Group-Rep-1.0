08daed6c5e4e30906a415189abdf3f2f6f37af6a
TristanDS95 <tdsahagian@gmail.com>
2026-01-29 18:48:56 -0500

    Add files via upload
    python code for the first data set generation - includes config file into a util subfolder
    
    also includes example csv file

---
+++ dock_proj_1/data_gen/util/config.py
@@ -0,0 +1,25 @@
+SMALL_CONFIG = {
+    "num_pages": 1_000,
+    "num_follows": 20_000,
+    "num_actions": 10_000
+}
+
+FULL_CONFIG = {
+    "num_pages": 200_000,
+    "num_follows": 20_000_000,
+    "num_actions": 10_000_000
+}
+
+HOBBIES = [
+    "reading", "traveling", "cooking", "swimming", "hiking",
+    "gaming", "painting", "dancing", "gardening", "fishing",
+    "cycling", "yoga", "photography", "writing", "knitting",
+    "birdwatching", "camping", "surfing", "skiing", "collecting",
+    "digging", "running", "baking", "woodworking", "scuba diving",
+    "guitar", "piano", "chess", "video games", "martial arts"
+]
+
+JOB_TITLES = [
+    "Engineer", "Worker", "Product Manager", "Laborer", "Plumber",
+    "Electrician", "Teacher", "Nurse", "Artist", "Musician", "Scientist"
+]
